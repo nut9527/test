c61dfcd589de0e478135fc1e31a5d936d630aa9e
nut9527 <gls280622@163.com>
2025-07-01 21:38:23 +0800

    Add files via upload

---
+++ 代码--Visual Studio Code/C++/test.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    int a=10;
+    float c=3.2;
+    cout<<a+c;
+    return 0;
+}
---
+++ 代码--Visual Studio Code/python/window.py
@@ -0,0 +1,6 @@
+from tkinter import *
+root=Tk()
+root.wm_title("这是一个窗口")
+wl=Label(root,text="欢迎")
+wl.pack()
+root.mainloop
