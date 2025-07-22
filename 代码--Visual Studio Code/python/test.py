eda4ba75baf5e06a78801a8c4fbfdefe4aab70ac
nut9527 <gls280622@163.com>
2025-07-23 02:02:19 +0800

    Delete 代码--Visual Studio Code directory

--- 代码--Visual Studio Code/python/test.py
+++
@@ -1,2 +0,0 @@
-a = {x for x in 'abracadabra' if x not in 'abc'}
-print(a);
--- 代码--Visual Studio Code/python/window.py
+++
@@ -1,6 +0,0 @@
-from tkinter import *
-root=Tk()
-root.wm_title("这是一个窗口")
-wl=Label(root,text="欢迎")
-wl.pack()
-root.mainloop
